921ea4fd384be776130dfb290e042c149bf4078b
r_fujisaki <ryusukefujisaki@gmail.com>
2023-04-18 20:18:39 +0900

    Making an interactive component

--- src/pages/tutorial/Tutorial.tsx
+++ src/pages/tutorial/Tutorial.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import './tutorial.css';
 
-type squareProps = {
+interface squareProps {
   value: number
 }
-class Square extends React.Component<squareProps, {}> {
+interface squareState {
+  value: string | null
+}
+class Square extends React.Component<squareProps, squareState> {
+  constructor(props: squareProps) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
